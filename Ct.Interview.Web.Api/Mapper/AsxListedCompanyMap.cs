734b6ad45f24de8c4e808f643322074d9567d691
Renz Natividad <zernnnnnnn@gmail.com>
2022-06-14 19:18:50 +0800

    Added ClassMap

---
+++ Ct.Interview.Web.Api/Mapper/AsxListedCompanyMap.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration;
+
+namespace Ct.Interview.Web.Api.Mapper
+{
+    public class AsxListedCompanyMap : ClassMap<AsxListedCompany>
+    {
+        public AsxListedCompanyMap()
+        {
+            Map(m => m.CompanyName).Name("Company name");
+            Map(m => m.AsxCode).Name("ASX code");
+            Map(m => m.GicsIndustryGroup).Name("GICS industry group");
+        }
+    }
+}
