ab6944f48959899077c79b079cccccce1c165617
Renz Natividad <zernnnnnnn@gmail.com>
2022-06-14 19:18:29 +0800

    Added Service Lifetime and Swagger

--- Ct.Interview.Web.Api/Program.cs
+++ Ct.Interview.Web.Api/Program.cs
@@ -1,17 +1,33 @@
+using Ct.Interview.Web.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAsxListedCompaniesService, AsxListedCompaniesService>();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.MapControllers();
+
 // Configure the HTTP request pipeline.
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json",
+        "Listed Companies API v1");
+});
+
+app.Run();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
