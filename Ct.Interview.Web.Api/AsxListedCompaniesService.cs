2134a0c85c6da73988820938b54f778ed7f2c637
Renz Natividad <zernnnnnnn@gmail.com>
2022-06-15 00:56:15 +0800

    Refactored Code to meet the Requirements

--- Ct.Interview.Web.Api/AsxListedCompaniesService.cs
+++ Ct.Interview.Web.Api/AsxListedCompaniesService.cs
@@ -8,16 +8,22 @@ namespace Ct.Interview.Web.Api
     public class AsxListedCompaniesService : IAsxListedCompaniesService
     {
         private readonly HttpClient client = new();
+        private readonly IConfiguration _configuration;
+
+        public AsxListedCompaniesService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public Task<AsxListedCompany[]> GetByAsxCode(string asxCode)
         {
             AsxListedCompany[] asxListedCompanies = Array.Empty<AsxListedCompany>();
 
-            var json = RunAsync(asxCode).GetAwaiter().GetResult();
+            var stream = RunAsync().GetAwaiter().GetResult();
 
-            if (json != null)
+            if (stream != null)
             {
-                using (var reader = new StreamReader(json))
+                using (var reader = new StreamReader(stream))
                 using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
                 {
                     for (int i = 0; i < 2; i++)
@@ -29,37 +35,22 @@ namespace Ct.Interview.Web.Api
 
                     csvReader.Context.RegisterClassMap<AsxListedCompanyMap>();
 
-                    asxListedCompanies = csvReader.GetRecords<AsxListedCompany>().ToArray();
+                    asxListedCompanies = csvReader.GetRecords<AsxListedCompany>().Where(x => String.Equals(x.AsxCode, asxCode, StringComparison.CurrentCultureIgnoreCase)).ToArray();
                 }
             }
             return Task.FromResult(asxListedCompanies);
         }
 
-        private async Task<Stream> RunAsync(string csvFile)
+        private async Task<Stream> RunAsync()
         {
-            client.BaseAddress = new Uri("https://www.asx.com.au/asx/research/");
+            var uri = new Uri(_configuration["AsxSettings:ListedSecuritiesCsvUrl"]);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             Stream stream = new MemoryStream();
 
-            try
-            {
-                stream = await GetCsvAsync(csvFile).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            HttpResponseMessage response = await client.GetAsync(uri);
 
-            return stream;
-        }
-
-        private async Task<Stream> GetCsvAsync(string csvFile)
-        {
-            Stream stream = new MemoryStream();
-
-            HttpResponseMessage response = await client.GetAsync(csvFile);
             if (response.IsSuccessStatusCode)
             {
                 stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
@@ -68,6 +59,7 @@ namespace Ct.Interview.Web.Api
             {
                 Console.WriteLine(response.ToString());
             }
+
             return stream;
         }
     }
